6dadd2bee206071f84d6b0a2bb1a9d0ad711fe33
Mahir Alvi Ridvi <ridvi123@gmail.com>
2025-08-18 23:39:34 +0600

    4.2

---
+++ chapter_4/4.2_layer_normalization.py
@@ -0,0 +1,20 @@
+import tiktoken
+import torch 
+import torch.nn as nn
+
+tokenizer=tiktoken.get_encoding('gpt2')
+
+batch=[]
+txt1='Every effort moves you'
+txt2='Every day holds a'
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+
+batch=torch.stack(batch,dim=0)
+
+torch.manual_seed(123)
+batch_example=torch.randn(2,5)
+layer=nn.Sequential(nn.Linear(5,6),nn.ReLU())
+out=layer(batch_example)
+print(out)
