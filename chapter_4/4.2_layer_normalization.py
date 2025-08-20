9fcc9fad2ee0aefa83c72c7f95e5549e24aa1ea0
Mahir Alvi Ridvi <ridvi123@gmail.com>
2025-08-20 22:13:16 +0600

    listing 4.2

--- chapter_4/4.2_layer_normalization.py
+++ chapter_4/4.2_layer_normalization.py
@@ -17,6 +17,28 @@ torch.manual_seed(123)
 batch_example=torch.randn(2,5)
 layer=nn.Sequential(nn.Linear(5,6),nn.ReLU())
 out=layer(batch_example)
-print(out)
 
-#4.2 #layer normalization
+mean=out.mean(dim=-1,keepdim=True)
+var=out.var(dim=-1,keepdim=True)
+torch.set_printoptions(sci_mode=False)
+out_norm=(out-mean)/torch.sqrt(var)
+mean=out_norm.mean(dim=-1,keepdim=True)
+var=out_norm.var(dim=-1,keepdim=True)
+
+#listing 4.2 a layer normalization class
+
+class LayerNorm(nn.Module):
+    def __init__(self,emb_dim):
+        super().__init__()
+        self.eps=1e-5
+        self.scale=nn.Parameter(torch.ones(emb_dim))
+        self.shift=nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self,x):
+        mean=x.mean(dim=-1,keepdim=True)
+        var=x.var(dim=-1,keepdim=True,unbiased=False)
+        norm_x=(x-mean)/torch.sqrt(var+self.eps)
+        return self.scale*norm_x+self.shift
+
+
+
