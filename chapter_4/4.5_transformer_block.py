58042e9e46d64671ee66d009a34a6f10bf73aab5
Mahir Alvi Ridvi <ridvi123@gmail.com>
2025-10-25 23:43:05 +0600

    generating text

--- chapter_4/4.5_transformer_block.py
+++ chapter_4/4.5_transformer_block.py
@@ -97,14 +97,49 @@ import tiktoken
 
 tokenizer=tiktoken.get_encoding('gpt2')
 
-batch=[]
-txt1='Every effort moves you'
-txt2='Every day holds a'
+# batch=[]
+# txt1='Every effort moves you'
+# txt2='Every day holds a'
 
-batch.append(torch.tensor(tokenizer.encode(txt1)))
-batch.append(torch.tensor(tokenizer.encode(txt2)))
-batch=torch.stack(batch,dim=0)
+# batch.append(torch.tensor(tokenizer.encode(txt1)))
+# batch.append(torch.tensor(tokenizer.encode(txt2)))
+# batch=torch.stack(batch,dim=0)
 
-out=model(batch)
+# out=model(batch)
 
-print(out.shape)
+# print(out.shape)
+
+
+
+#4.7 generating text
+
+
+#listing 4.8 function for gpt model to generate text
+
+def generating_text_simple(model,idx,max_new_tokens,context_size):
+    for _ in range(max_new_tokens):
+        idx_cond=idx[:,-context_size:]
+        with torch.no_grad():
+            logits=model(idx_cond)
+
+        logits=logits[:,-1,:]
+        probas=torch.softmax(logits,dim=-1)
+        idx_next=torch.argmax(probas,dim=-1,keepdim=True)
+        idx=torch.cat((idx,idx_next),dim=-1)
+
+    return idx
+
+start_context="Hello, I am"
+encoded=tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor=torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval()
+out=generating_text_simple(model=model,idx=encoded_tensor,max_new_tokens=6,context_size=GPT_CONFIG_124M['context_length'])
+
+print('output: ',out)
+print("output_length:",len(out[0]))
+
+decoded_text=tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
